83c2fc0c4492568a94ba9312ab294ec96e2d6f60
markwbennett <mb@ivi3.com>
2025-06-14 14:48:09 -0500

    Update formula to use proper GitHub release with app-only ZIP

--- Casks/iacls-time-tracker.rb
+++ Casks/iacls-time-tracker.rb
@@ -2,13 +2,13 @@ cask "iacls-time-tracker" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/markwbennett/TimeTrackButton/archive/refs/heads/main.zip"
+  url "https://github.com/markwbennett/TimeTrackButton/releases/download/v#{version}/IACLS_Time_Tracker_App.zip"
   name "IACLS Time Tracker"
   desc "macOS time tracking GUI application with floating button interface"
   homepage "https://github.com/markwbennett/TimeTrackButton"
 
-  # Only install the GUI app bundle
-  app "TimeTrackButton-main/IACLS Time Tracker.app"
+  # Install the GUI app bundle
+  app "IACLS Time Tracker.app"
 
   caveats <<~EOS
     IACLS Time Tracker GUI app has been installed.
